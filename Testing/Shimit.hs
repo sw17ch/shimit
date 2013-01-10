9cb8073a00fa75f283706dd820d56691420f9c1c
John Van Enk <vanenkj@gmail.com>
2013-01-09 22:41:01 -0500

    Okay, I'm happy with it now. It outputs a list of dependent files for all top-level functions.

--- Testing/Shimit.hs
+++ Testing/Shimit.hs
@@ -2,18 +2,30 @@
 module Testing.Shimit where
 
 import Data.Generics.Schemes
-import Data.Map (Map)
+import Data.List
+import Data.Map (Map, (!))
+import Data.Maybe
 import Language.C
 import Language.C.Analysis
 
 getNeededTypes :: CTranslUnit -> [CTypeSpecifier NodeInfo]
 getNeededTypes ast = concatMap relevantTypes $ topLevelFunDefs ast
 
--- getRequiredFiles :: CTranslUnit -> [String]
--- getRequiredFiles ast = 
---   where
---     needed_types = getNeededTypes ast
---     (tags,typedefs) = typeMaps ast
+getRequiredFiles :: CTranslUnit -> [String]
+getRequiredFiles ast = nub $ catMaybes $ map defPos needed_types
+  where
+    defPos (CTypeDef n _) = let (TypeDef _ _ _ i) = typedefs ! n in Just $ nodePos i
+    defPos (CSUType (CStruct _ (Just i) _ _ _) _) = Just $ nodePos $ infoByIdent i
+    defPos (CEnumType (CEnum (Just i) _ _ _) _) = Just $ nodePos $ infoByIdent i
+    defPos _ = Nothing
+
+    nodePos = posFile . posOf
+    needed_types = getNeededTypes ast
+    (tags,typedefs) = typeMaps ast
+    infoByIdent i = let ref = NamedRef i
+                    in case tags ! ref of
+                         CompDef (CompType _ _ _ _ i') -> i'
+                         EnumDef (EnumType _ _ _ i') -> i'
 
 typeMaps :: CTranslUnit -> (Map SUERef TagDef, Map Ident TypeDef)
 typeMaps ast = case runTrav () (analyseAST ast >>= (\a -> return (gTags a, gTypeDefs a))) of
--- shimit.hs
+++ shimit.hs
@@ -1,13 +1,10 @@
 {-# LANGUAGE DeriveDataTypeable, StandaloneDeriving, ViewPatterns #-}
-module Main where
+module Main (main) where
 
 import Testing.Shimit
 import System.Console.CmdArgs
 import Language.C
 import Language.C.System.GCC
-import Language.C.Analysis
-import Data.Map (Map, (!))
-import Data.Maybe
 
 data Options = Options { inputFile :: FilePath
                        } deriving (Data, Typeable, Show)
@@ -22,24 +19,6 @@ main = do
     parsed <- parseCFile (newGCC "gcc") Nothing [] (inputFile opts)
 
     case parsed of
-      Right ast -> inspect ast
+      Right ast -> print $ getRequiredFiles ast
       Left err -> putStrLn $ "ERROR: " ++ show err
 
-inspect :: CTranslUnit -> IO ()
-inspect ast = do
-  let needed_types = getNeededTypes ast
-  let tms = typeMaps ast
-
-  mapM_ print $ catMaybes $ map (dependOnHrds tms) needed_types
-
-dependOnHrds :: (Map SUERef TagDef, Map Ident TypeDef) -> CTypeSpecifier NodeInfo -> Maybe NodeInfo
-dependOnHrds (_, gtds) (CTypeDef n _) = let (TypeDef _ _ _ i) = gtds ! n in Just i
-dependOnHrds (gts, _) (CSUType (CStruct _ (Just i) _ _ _) _) = Just $ lookupInfoByIdent gts i
-dependOnHrds (gts, _) (CEnumType (CEnum (Just i) _ _ _) _) = Just $ lookupInfoByIdent gts i
-dependOnHrds _ _ = Nothing
-
-lookupInfoByIdent :: Map SUERef TagDef -> Ident -> NodeInfo
-lookupInfoByIdent gts i = let ref = NamedRef i
-                          in case gts ! ref of
-                               CompDef (CompType _ _ _ _ i') -> i'
-                               EnumDef (EnumType _ _ _ i') -> i'
